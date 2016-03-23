d171787cbf18083302dacbbddedfa48135b08087
Tyler Wong <tbwong3@gmail.com>
2016-03-22 21:59:12 -0700

    locked in portrait

--- app/src/main/java/com/example/tylerbwong/cardgame/SplashActivity.java
+++ app/src/main/java/com/example/tylerbwong/cardgame/SplashActivity.java
@@ -12,40 +12,44 @@ import android.widget.TextView;
  * Created by brittanyberlanga on 3/21/16.
  */
 public class SplashActivity extends AppCompatActivity {
-    private static final int SPLASH_DISPLAY_LENGTH = 1000;
+   private static final int SPLASH_DISPLAY_LENGTH = 1000;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.splash_activity);
-        TextView title = (TextView) findViewById(R.id.title_label);
-        Typeface gotham = Typeface.createFromAsset(getAssets(), "font/gotham-light.ttf");
-        title.setTypeface(gotham);
+      setContentView(R.layout.splash_activity);
+      TextView title = (TextView) findViewById(R.id.title_label);
+      Typeface gotham = Typeface.createFromAsset(getAssets(), "font/gotham-light.ttf");
+      title.setTypeface(gotham);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+      new Handler().postDelayed(new Runnable() {
+         @Override
+         public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                SplashActivity.this.finish();
-            }
-        }, SPLASH_DISPLAY_LENGTH);
-    }
+            Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+            SplashActivity.this.startActivity(mainIntent);
+            SplashActivity.this.finish();
+         }
+      }, SPLASH_DISPLAY_LENGTH);
+   }
 
-    @Override
-    public void onWindowFocusChanged(boolean hasFocus) {
-        super.onWindowFocusChanged(hasFocus);
-        View decorView = getWindow().getDecorView();
-        if (hasFocus) {
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-    }
+   @Override
+   public void onWindowFocusChanged(boolean hasFocus) {
+      super.onWindowFocusChanged(hasFocus);
+      if (hasFocus) {
+         setFullscreen();
+      }
+   }
+
+   private void setFullscreen() {
+      View decorView = getWindow().getDecorView();
+      decorView.setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                  | View.SYSTEM_UI_FLAG_FULLSCREEN
+                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+   }
 }
