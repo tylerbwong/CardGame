af6d3e0030bf1d762bc92250f3b389208e20bb90
Tyler Wong <tbwong3@gmail.com>
2016-03-22 19:20:39 -0700

    formatted driver

--- app/src/main/java/com/example/tylerbwong/cardgame/magictrick/MagicDriver.java
+++ app/src/main/java/com/example/tylerbwong/cardgame/magictrick/MagicDriver.java
@@ -11,18 +11,15 @@ import java.util.Scanner;
  */
 public class MagicDriver {
    public static void printArray(ArrayList<Card> array) {
-      int index = 0;
       for (Card card : array) {
-         System.out.println(index++ + ": " + card.toString());
+         System.out.println(card.toString());
       }
    }
 
    public static void printPiles(ArrayList<Card>[] piles) {
       for (int index = 0; index < piles.length; index++) {
          System.out.println("Choice " + index);
-         for (Card card : piles[index]) {
-            System.out.println(card.toString());
-         }
+         printArray(piles[index]);
          System.out.println("--------------------");
       }
    }
@@ -31,23 +28,37 @@ public class MagicDriver {
       Scanner scan = new Scanner(System.in);
       System.out.println("Would you like to see a magic trick? (1 - yes, 2 - no)");
       int yesNo = scan.nextInt();
+
       if (yesNo == 1) {
+         // make deck and trick objects
          Deck deck = new Deck();
          MagicTrick trick = new MagicTrick(deck);
+
+         // get user number selection
          System.out.println("Enter a number between 0 and 27");
          int cardChoice = scan.nextInt();
          trick.setChoice(cardChoice);
-         System.out.println("Pick one of these cards and don't forget!");
+
+         // print out cards
+         System.out.println("Remember one of these cards and don't forget!");
          ArrayList<Card> trickDeck = trick.getTrickDeck();
          printArray(trickDeck);
-         while (trick.getStage() < 3) {
-            trick.dealToPiles();
-            System.out.println("Which pile is your card in? (0 - 2)");
-            printPiles(trick.getPiles());
-            int decision = scan.nextInt();
-            if (trick.verifyChoice(decision))
-            {
-               trick.returnPilesToDeck();
+         System.out.println("Press 1 when ready!");
+         int answer = scan.nextInt();
+
+         // answer was 1
+         if (answer == 1) {
+            // while still dealing to piles
+            while (trick.getStage() < 3) {
+               trick.dealToPiles();
+               System.out.println("Which pile is your card in? (0 - 2)");
+               printPiles(trick.getPiles());
+
+               // verify choice
+               int decision = scan.nextInt();
+               if (trick.verifyChoice(decision)) {
+                  trick.returnPilesToDeck();
+               }
             }
          }
          System.out.println("Was your card the " + trick.getSolution().toString());
