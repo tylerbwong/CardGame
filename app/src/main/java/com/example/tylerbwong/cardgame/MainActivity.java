6d730551f6d5b585d27985f4c59ca004f7ebe127
Tyler Wong <tbwong3@gmail.com>
2016-03-22 21:46:58 -0700

    fixed exit error

--- app/src/main/java/com/example/tylerbwong/cardgame/MainActivity.java
+++ app/src/main/java/com/example/tylerbwong/cardgame/MainActivity.java
@@ -93,8 +93,10 @@ public class MainActivity extends AppCompatActivity {
     * @param v
     */
    public void exitAction(View v) {
-      Process.killProcess(Process.myPid());
-      System.exit(1);
+      Intent intent = new Intent(Intent.ACTION_MAIN);
+      intent.addCategory(Intent.CATEGORY_HOME);
+      intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+      startActivity(intent);
    }
 
    @Override
