fb04f12711ce0263747a3586c979393bcbd371cc
brittanyberlanga <brittany.berlanga@gmail.com>
2016-03-22 17:37:49 -0700

    added splash screen

--- app/src/main/java/com/example/tylerbwong/cardgame/MainActivity.java
+++ app/src/main/java/com/example/tylerbwong/cardgame/MainActivity.java
@@ -2,12 +2,12 @@ package com.example.tylerbwong.cardgame;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Process;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.os.Process;
 
 public class MainActivity extends AppCompatActivity {
    private TextView titleLabel;
@@ -103,12 +103,12 @@ public class MainActivity extends AppCompatActivity {
       View decorView = getWindow().getDecorView();
       if (hasFocus) {
          decorView.setSystemUiVisibility(
-               View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                     | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                     | View.SYSTEM_UI_FLAG_FULLSCREEN
-                     | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                         | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                         | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                         | View.SYSTEM_UI_FLAG_FULLSCREEN
+                         | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
       }
    }
 }
