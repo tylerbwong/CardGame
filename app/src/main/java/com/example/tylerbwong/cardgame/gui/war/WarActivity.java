86a33c5d4d527a72813ff6d2ac64d6c8e501126b
brittanyberlanga <brittany.berlanga@gmail.com>
2016-03-24 22:05:50 -0700

    fixed war dialog from crashing when font is changed

--- app/src/main/java/com/example/tylerbwong/cardgame/gui/war/WarActivity.java
+++ app/src/main/java/com/example/tylerbwong/cardgame/gui/war/WarActivity.java
@@ -1,27 +1,19 @@
 package com.example.tylerbwong.cardgame.gui.war;
 
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.example.tylerbwong.cardgame.R;
-import com.example.tylerbwong.cardgame.gui.mainmenu.MainActivity;
-
-import android.app.AlertDialog;
-import android.text.method.PasswordTransformationMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.example.tylerbwong.cardgame.R;
+import com.example.tylerbwong.cardgame.gui.mainmenu.MainActivity;
 
 /**
  * Created by tylerbwong on 3/24/16.
@@ -60,6 +52,7 @@ public class WarActivity extends AppCompatActivity {
       View alertLayout = inflater.inflate(R.layout.dialog_view, null);
       alert.setView(alertLayout);
       AlertDialog dialog = alert.create();
+      dialog.create();
       Typeface gotham = Typeface.createFromAsset(getAssets(), "font/gotham-light.ttf");
       Button noButton = dialog.getButton(Dialog.BUTTON_NEGATIVE);
       noButton.setTypeface(gotham);
